6d9a338eb4626ba6cb46f2e98e9ba33ebd74bc1e
legendary11 <mtlong2s@semo.edu>
2016-04-26 10:38:41 -0500

    role management

--- ContractGenerator/ContractGenerator/ContractGenerator/ContractGenerator/ContractGenerator/ViewContract.aspx.cs
+++ ContractGenerator/ContractGenerator/ContractGenerator/ContractGenerator/ContractGenerator/ViewContract.aspx.cs
@@ -11,7 +11,32 @@ namespace ContractGenerator
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-           
+           string[] roles = Roles.GetRolesForUser();
+
+            if (roles.Contains<string>("admin"))
+            {
+                SqlDataSource1.SelectCommand = "SELECT UserName, ContractTitle, DomainName, CreatedDate, ModifiedDate, CreateBy FROM Contract, Users WHERE Users.UserID = Contract.UserID AND Status = 'false'";
+                GridView1.DataBind();
+                SqlDataSource1.SelectCommand = "SELECT UserName, ContractTitle, DomainName, CreatedDate, ModifiedDate, CreateBy FROM Contract, Users WHERE Users.UserID = Contract.UserID AND Status = 'true'";
+                GridView1.DataBind();
+
+            }
+            else if (roles.Contains<string>("staff"))
+            {
+                SqlDataSource1.SelectCommand = "SELECT UserName, ContractTitle, DomainName, CreatedDate, ModifiedDate, CreateBy FROM Contract, Users WHERE Users.UserID = Contract.UserID AND Status = 'false' AND CreateUserID = '" + Session["UserID"] + "'";
+                GridView1.DataBind();
+                SqlDataSource1.SelectCommand = "SELECT UserName, ContractTitle, DomainName, CreatedDate, ModifiedDate, CreateBy FROM Contract, Users WHERE Users.UserID = Contract.UserID AND Status = 'true' AND CreateUserID = '" + Session["UserID"] + "'";
+                GridView1.DataBind();
+            }
+            else if (roles.Contains<string>("client"))
+            {
+                SqlDataSource1.SelectCommand = "SELECT UserName, ContractTitle, DomainName, CreatedDate, ModifiedDate, CreateBy FROM Contract, Users WHERE Users.UserID = Contract.UserID AND Status = 'false' AND UserID = '" + Session["UserID"] + "'";
+                GridView1.DataBind();
+                GridView1.Columns[0].Visible = false;
+                SqlDataSource1.SelectCommand = "SELECT UserName, ContractTitle, DomainName, CreatedDate, ModifiedDate, CreateBy FROM Contract, Users WHERE Users.UserID = Contract.UserID AND Status = 'true' AND UserID = '" + Session["UserID"] + "'";
+                GridView1.DataBind();
+                GridView1.Columns[0].Visible = false;
+            }
         }
     }
 }
